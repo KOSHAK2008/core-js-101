f48bffe758e3a61bdfcf07f5c3df089ca0c1b39e
KOSHAK2008 <antonkarko33@gmail.com>
2022-12-27 16:50:41 +0300

    feat: done tasks for date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,9 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value.valueOf());
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -34,8 +35,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
+  // throw new Error('Not implemented');
 }
 
 
@@ -74,6 +76,12 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(/* startDate, endDate */) {
+  // const time = Date.parse(endDate) - Date.parse(startDate);
+  // const hours = (time / (1000 * 3600)) % 24;
+  // const minutes = (time / 1000 / 60) % 60;
+  // const seconds = (time / 1000) % 60;
+  // const miliseconds = time % 1000;
+  // return `0${hours}:${minutes}:${seconds}.${miliseconds}`;
   throw new Error('Not implemented');
 }
 
